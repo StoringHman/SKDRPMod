49b0a9ec7a112c3188dc683491532496a32200b3
hman <188392580+StoringHman@users.noreply.github.com>
2025-11-08 17:16:10 +0000

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,5 +1,5 @@
 local modData = {
-  ["CurrentModVersion"] = 3
-  ["ModMenuVersion"] = 1
-  ["DefaultPathLists"] = {"skdrpmod", "skdrpmod2", "mountainiomod"}
+  ["CurrentModVersion"] = 3,
+  ["ModMenuVersion"] = 1,
+  ["DefaultPathLists"] = {"skdrpmod", "skdrpmod2", "mountainiomod", "betamariomod"}
 }
